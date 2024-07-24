346b5a28119313c64470631b24a85375f7558731
Stephen Hara <stephen.hara@stephenhara.com>
2024-07-24 17:11:55 +0900

    some progress on rb tree

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const Color = enum { Black, Red };
 
 const RBTree = struct {
     value: ?i32,
-    color: ?bool = Color.Black,
+    color: ?Color = Color.Black,
     left: ?*RBTree = undefined,
     right: ?*RBTree = undefined,
 
@@ -12,6 +12,15 @@ const RBTree = struct {
         return RBTree{
             .value = null,
         };
+    }
+
+    pub fn insert(self: *RBTree, value: i32) !void {
+        if (self.value == null) {
+            self.value = value;
+            return;
+        }
+
+        if (self.value > value) {}
     }
 };
 
@@ -25,9 +34,21 @@ test "rb tree init" {
     try std.testing.expectEqual(bst_root.color, Color.Black);
 }
 
-test "rb tree insert " {
-    const bst_root = RBTree{
+test "rb tree insert with empty tree" {
+    var bst_root = RBTree{
         .value = null,
     };
     try std.testing.expectEqual(bst_root.value, null);
+
+    try bst_root.insert(5);
+    try std.testing.expectEqual(bst_root.value, 5);
+}
+
+test "rb tree multiple inserts" {
+    var bst_root = RBTree{
+        .value = null,
+    };
+    try bst_root.insert(5);
+    try bst_root.insert(3);
+    try bst_root.insert(10);
 }
